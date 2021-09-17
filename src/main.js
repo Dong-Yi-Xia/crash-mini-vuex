9c0acecfc8d532a431af146f85933c73bc98952e
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-09-17 16:43:47 -0400

    boilderplate

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+// can just write './store' instead of './store/index.js' since its a index file
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store/modules/todos.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const state = {};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+//Because of ES6 rather than exporting the object such as state: state, just write it one time.
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
